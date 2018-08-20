7d4212f2a896315020f172c1852ce1e7af63463d
Thomas E Beach <thomas.beach@flatironschool.com>
2018-08-20 00:29:15 +0000

    automatically backed up by learn

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", e => {
+  console.log("== DOMContentLoaded ==")
   document.querySelector("body")
    .addEventListener("click", e => console.log("Reggae, Reggae!"));
 })
